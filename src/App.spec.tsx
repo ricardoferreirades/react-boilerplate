d5ce0f3500619abdbc1aa7a6f6b46e95ec868ec9
Ricardo Ferreira <ricardoferreirades@gmail.com>
2022-08-10 20:14:03 +0200

    test: configure styled component tests for snapshot
    Signed-off-by: Ricardo Ferreira <ricardoferreirades@gmail.com>

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,10 +1,19 @@
 import React from "react";
-import { render, unmountComponentAtNode } from "react-dom";
+import { createRoot } from "react-dom/client";
 import { act } from "react-dom/test-utils";
 import App from "./App";
+import styled from "styled-components";
+import renderer from "react-test-renderer";
+import "jest-styled-components";
+
+const Button = styled.button`
+  color: green;
+`;
 
 describe("App Component", () => {
   let container: any = null;
+  let root;
+
   beforeEach(() => {
     // setup a DOM element as a render target
     container = document.createElement("div");
@@ -13,15 +22,23 @@ describe("App Component", () => {
 
   afterEach(() => {
     // cleanup on exiting
-    unmountComponentAtNode(container);
+    act(() => {
+      root.unmount(container);
+    });
     container.remove();
     container = null;
   });
 
   it("works", () => {
     act(() => {
-      render(<App />, container);
+      root = createRoot(container);
+      root.render(<App />);
     });
-    expect(container.textContent).toBe("Hello, Ricardo Gostoso!");
+    expect(container.textContent).toBe("Hello, Ricardo Gostoso, MARAVILHOSO!");
+  });
+
+  it("works", () => {
+    const tree = renderer.create(<Button>Foo</Button>);
+    expect(tree).toMatchSnapshot();
   });
 });
