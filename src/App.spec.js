ec26bc0c920e8c59419036582fb2f21d1c80bfc9
Ricardo Ferreira <ricardoferreirades@gmail.com>
2022-08-10 18:55:39 +0200

    test: setup tests
    Signed-off-by: Ricardo Ferreira <ricardoferreirades@gmail.com>

---
+++ src/App.spec.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+import App from "./App";
+
+describe("App Component", () => {
+  let container = null;
+  beforeEach(() => {
+    // setup a DOM element as a render target
+    container = document.createElement("div");
+    document.body.appendChild(container);
+  });
+
+  afterEach(() => {
+    // cleanup on exiting
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null;
+  });
+
+  it("works", () => {
+    act(() => {
+      render(<App />, container);
+    });
+    expect(container.textContent).toBe("Hello, Ricardo Gostoso!");
+  });
+});
